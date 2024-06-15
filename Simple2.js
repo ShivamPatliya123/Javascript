d733e5d2cac2370107bc7acfb5794ba668345ef4
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-06-15 20:24:05 +0530

    Simple2.js

---
+++ Simple2.js
@@ -0,0 +1,15 @@
+const a ={
+    name:"shiva",
+    age:20
+}
+
+a.name = "shyam"
+a.age = 21
+
+console.log(a)
+// a = 24  we are not change constant varibable
+// console.log(a)
+
+let dic = ['spiritual','beat','change','Attack','instrument']
+console.log(dic)
+console.log(typeof(dic))
